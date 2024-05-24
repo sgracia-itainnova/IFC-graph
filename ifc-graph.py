b87e8a2cca4033d9be240dd1f55468a27560a97d
sgracia <sgracia@itainnova.es>
2024-05-24 15:08:06 +0200

    Added plot representation without DB connection

--- ifc-graph.py
+++ ifc-graph.py
@@ -1,17 +1,37 @@
-
 import argparse
-import os
-from dotenv import load_dotenv
-
-from neo4j import GraphDatabase
-from py2neo import Graph
-
 import ifcopenshell
+import networkx as nx
+from pyvis.network import Network
 
 import p2n
 import opd
 
-load_dotenv()
+def plot_large_graph(graph, output_file):
+    # Initialize Pyvis network
+    net = Network(notebook=False, height="1000px", width="100%", bgcolor="#222222", font_color="white")
+
+    # Add nodes with labels
+    print('Adding nodes to plot')
+    for i, (node, data) in enumerate(graph.nodes(data=True)):
+        net.add_node(node, label=data.get('name', ''), title=str(data))
+        print(f'Added node {i}/{len(graph.nodes)}')
+
+    # Add edges with labels
+    print('Adding edges to plot')
+    for i, (source, target, data) in enumerate(graph.edges(data=True)):
+        net.add_edge(source, target, label=data.get('type', ''), title=str(data))
+        print(f'Added edge {i}/{len(graph.edges)}')
+
+    # Save the interactive plot to the specified file
+    net.save_graph(output_file)
+
+def save_graph(graph, filename):
+    nx.write_edgelist(graph, filename)
+
+def load_graph(filename):
+    return nx.read_edgelist(filename)
+
+ONLY_PLOT = True
 
 # Press the green button in the gutter to run the script.
 if __name__ == "__main__":
@@ -26,47 +46,38 @@ if __name__ == "__main__":
     parser.add_argument('-a', '--algorithm', type=str,
                         help='Write "all" to test all algorithms. Write "zhu" or "wiss" to test other algorithms',
                         default='all')
+    parser.add_argument('-o', '--output', type=str, help='Output HTML file for the graph visualization',
+                        default='ifc_graph.html')
+    parser.add_argument('-g', '--graph', type=str, help='Output file for saving the graph object',
+                        default='ifc_graph.txt')
     args = parser.parse_args()
 
-    # Parse IFC STEP file using IfcOpenShell
-    print("Use IfcOpenShell to parse IFC STEP file.")
-    my_ifc_file = ifcopenshell.open(args.file)
+    if not ONLY_PLOT:
+        # Parse IFC STEP file using IfcOpenShell
+        print("Use IfcOpenShell to parse IFC STEP file.")
+        my_ifc_file = ifcopenshell.open(args.file)
 
-    # Connect to Neo4j database for Zhu py2neo algorithm
-    print("Connecting to neo4j_py2neo using py2neo...")
-    print("NEO4J_PY2NEO_URI", os.getenv('NEO4J_PY2NEO_URI'))
-    print("NEO4J_USER", os.getenv('NEO4J_USER'))
-    print("NEO4J_INITIAL_PASSWORD", os.getenv('NEO4J_INITIAL_PASSWORD'))
-    try:
-        py2neo_graph = Graph(os.getenv('NEO4J_PY2NEO_URI'),
-                             auth=(os.getenv('NEO4J_USER'),
-                                   os.getenv('NEO4J_INITIAL_PASSWORD')))
-        py2neo_graph.run("MATCH (n) DETACH DELETE n")
-        py2neo_graph.run("MATCH () RETURN 1 LIMIT 1")
-        print('Connected.')
-    except Exception:
-        print('Could not establish connection.')
+        # Create an in-memory graph using NetworkX
+        G = nx.Graph()
 
-    # Connect to Neo4j database for Wiss official driver algorithm
-    print("Connecting to neo4j_official using official driver...")
-    print("NEO4J_OFFICIAL_URI", os.getenv('NEO4J_OFFICIAL_URI'))
-    print("NEO4J_USER", os.getenv('NEO4J_USER'))
-    print("NEO4J_INITIAL_PASSWORD", os.getenv('NEO4J_INITIAL_PASSWORD'))
-    try:
-        official_graph_driver = GraphDatabase.driver(os.getenv('NEO4J_OFFICIAL_URI'),
-                                                     auth=(os.getenv('NEO4J_USER'),
-                                                           os.getenv('NEO4J_INITIAL_PASSWORD')))
-        official_graph_driver.verify_connectivity()
-        official_graph_driver.execute_query("MATCH (n) DETACH DELETE n")
-        official_graph_driver.execute_query("MATCH () RETURN 1 LIMIT 1")
-        print('Connected.')
-    except Exception:
-        print('Could not establish connection.')
+        # Insert using Zhu algorithm
+        if args.algorithm in ('all', 'zhu'):
+            p2n.create_full_graph(G, my_ifc_file)
 
-    # Insert using Zhu py2neo algorithm
-    if args.algorithm in ('all', 'zhu'):
-        p2n.create_full_graph(py2neo_graph, my_ifc_file)
+        # Insert using Wiss algorithm
+        if args.algorithm in ('all', 'wiss'):
+            opd.create_full_graph(G, my_ifc_file)
 
-    # Insert using Wiss official driver algorithm
-    if args.algorithm in ('all', 'wiss'):
-        opd.create_full_graph(official_graph_driver, my_ifc_file)
+        # Cleanup IFC file
+        del my_ifc_file
+
+        # Print the created graph
+        print("Graph created:")
+
+        # Save the graph object
+        save_graph(G, args.graph)
+        print(f"Graph object saved to {args.graph}")
+
+    # Plot the graph using Pyvis and save it to the specified file
+    G = load_graph(args.graph)
+    plot_large_graph(G, args.output)
--- p2n.py
+++ p2n.py
@@ -18,62 +18,61 @@
 # Some modifications have been made to the code.
 #
 
-from py2neo.data import Node, Relationship
+import networkx as nx
 from uuid import uuid4
 
-# Create the basic node with literal attributes and the class hierarchy
 def create_pure_node_from_ifc_entity(ifc_entity, ifc_file, hierarchy=True):
-    node = Node()
-    if ifc_entity.id() != 0:
-        node['id'] = ifc_entity.id()
-    else:
-        node['id'] = str(uuid4())
-    node['name'] = ifc_entity.is_a()
+    node = {
+        'id': ifc_entity.id() if ifc_entity.id() != 0 else str(uuid4()),
+        'name': ifc_entity.is_a()
+    }
     if hierarchy:
+        labels = []
         for label in ifc_file.wrapped_data.types_with_super():
             if ifc_entity.is_a(label):
-                node.add_label(label)
+                labels.append(label)
+        node['labels'] = labels
     else:
-        node.add_label(ifc_entity.is_a())
+        node['labels'] = [ifc_entity.is_a()]
+    
     attributes_type = ['ENTITY INSTANCE', 'AGGREGATE OF ENTITY INSTANCE', 'DERIVED']
     for i in range(ifc_entity.__len__()):
         if not ifc_entity.wrapped_data.get_argument_type(i) in attributes_type:
             name = ifc_entity.wrapped_data.get_argument_name(i)
             name_value = ifc_entity.wrapped_data.get_argument(i)
             node[name] = name_value
-    node.__primarylabel__ = 'Root'
-    node.__primarykey__ = 'id'
     return node
 
-
-# Process literal attributes, entity attributes, and relationship attributes
 def create_graph_from_ifc_entity_all(graph, ifc_entity, ifc_file):
-    node = create_pure_node_from_ifc_entity(ifc_entity, ifc_file)
-    graph.merge(node)
+    node_id = ifc_entity.id() if ifc_entity.id() != 0 else str(uuid4())
+    node_data = create_pure_node_from_ifc_entity(ifc_entity, ifc_file)
+    graph.add_node(node_id, **node_data)
     for i in range(ifc_entity.__len__()):
         if ifc_entity[i]:
             if ifc_entity.wrapped_data.get_argument_type(i) == 'ENTITY INSTANCE':
                 if ifc_entity[i].is_a() in ['IfcOwnerHistory'] and ifc_entity.is_a() != 'IfcProject':
                     continue
                 else:
-                    sub_node = create_pure_node_from_ifc_entity(ifc_entity[i], ifc_file)
-                    REL = Relationship(node, ifc_entity.wrapped_data.get_argument_name(i), sub_node)
-                    graph.merge(REL)
+                    sub_node_id = ifc_entity[i].id() if ifc_entity[i].id() != 0 else str(uuid4())
+                    sub_node_data = create_pure_node_from_ifc_entity(ifc_entity[i], ifc_file)
+                    graph.add_node(sub_node_id, **sub_node_data)
+                    graph.add_edge(node_id, sub_node_id, type=ifc_entity.wrapped_data.get_argument_name(i))
             elif ifc_entity.wrapped_data.get_argument_type(i) == 'AGGREGATE OF ENTITY INSTANCE':
                 for sub_entity in ifc_entity[i]:
-                    sub_node = create_pure_node_from_ifc_entity(sub_entity, ifc_file)
-                    REL = Relationship(node, ifc_entity.wrapped_data.get_argument_name(i), sub_node)
-                    graph.merge(REL)
+                    sub_node_id = sub_entity.id() if sub_entity.id() != 0 else str(uuid4())
+                    sub_node_data = create_pure_node_from_ifc_entity(sub_entity, ifc_file)
+                    graph.add_node(sub_node_id, **sub_node_data)
+                    graph.add_edge(node_id, sub_node_id, type=ifc_entity.wrapped_data.get_argument_name(i))
     for rel_name in ifc_entity.wrapped_data.get_inverse_attribute_names():
         if ifc_entity.wrapped_data.get_inverse(rel_name):
             inverse_relations = ifc_entity.wrapped_data.get_inverse(rel_name)
             for wrapped_rel_entity in inverse_relations:
                 rel_entity = ifc_file.by_id(wrapped_rel_entity.id())
-                sub_node = create_pure_node_from_ifc_entity(rel_entity, ifc_file)
-                REL = Relationship(node, rel_name, sub_node)
-                graph.merge(REL)
+                sub_node_id = rel_entity.id() if rel_entity.id() != 0 else str(uuid4())
+                sub_node_data = create_pure_node_from_ifc_entity(rel_entity, ifc_file)
+                graph.add_node(sub_node_id, **sub_node_data)
+                graph.add_edge(node_id, sub_node_id, type=rel_name)
     return
-
 
 def create_full_graph(graph, ifc_file):
     idx = 1
